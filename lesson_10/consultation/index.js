70cd8c1f4f508e900abc171ead8dfd0bfcccd580
s-saratov <saratovs@yandex.ru>
2024-12-05 19:16:09 +0100

    HW 10

---
+++ lesson_10/consultation/index.js
@@ -0,0 +1,28 @@
+
+
+
+console.log("11" - 22); // -11
+console.log("22" === 22); // false
+console.log(50 - undefined); // NaN
+console.log(50 + undefined); // NaN
+console.log("50" + undefined); // NaN
+
+
+console.log(50 / undefined); // NaN
+console.log(50 * undefined); // NaN
+
+console.log("message" * 3); // NaN
+console.log(50 - "message");    // NaN
+console.log(0 / 0); // NaN
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
