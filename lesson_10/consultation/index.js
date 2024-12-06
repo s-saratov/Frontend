c50e4987ec290c97d08829fc1d7179c3a1d51680
s-saratov <saratovs@yandex.ru>
2024-12-06 01:13:32 +0100

    consultation 06.12.2024

--- lesson_10/consultation/index.js
+++ lesson_10/consultation/index.js
@@ -1,28 +1,41 @@
+let result = 100 / 0;
+console.log(result); // Infinity
 
+// Оператор !! приводит переменную к типу boolean
 
+let firstName = "John";
+console.log(!!firstName); // true
+let lastName = "";
+console.log(!!lastName); // false
+
+let userData = {
+  fistName: "John",
+  lastName: "Johnson",
+};
+console.log(!!userData); // true
+
+let userData2 = undefined;
+console.log(!!userData2); // false
+
+if (!!userData === true) {
+    // Отображаем пришедшие с БД пользователя данные (автар, имя, фамилия и т.д) на странице
+  } else {
+    // Показываеи пользователю ошибку
+  }
 
 console.log("11" - 22); // -11
 console.log("22" === 22); // false
 console.log(50 - undefined); // NaN
 console.log(50 + undefined); // NaN
 console.log("50" + undefined); // NaN
-
-
 console.log(50 / undefined); // NaN
 console.log(50 * undefined); // NaN
 
 console.log("message" * 3); // NaN
-console.log(50 - "message");    // NaN
+console.log(50 - "message"); // NaN
 console.log(0 / 0); // NaN
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
+console.log(50 / null); // 0
+console.log(50 - true); // 49
 
+let someVariable;
+console.log(someVariable);  // undefined
